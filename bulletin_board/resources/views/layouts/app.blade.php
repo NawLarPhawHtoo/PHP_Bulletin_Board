189ace68c9f131802bf172a8b795855f85dc5c71
NawLarPhawHtoo <113975423+NawLarPhawHtoo@users.noreply.github.com>
2023-10-16 16:14:09 +0630

    commit changes

--- bulletin_board/resources/views/layouts/app.blade.php
+++ bulletin_board/resources/views/layouts/app.blade.php
@@ -24,6 +24,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+
+
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
